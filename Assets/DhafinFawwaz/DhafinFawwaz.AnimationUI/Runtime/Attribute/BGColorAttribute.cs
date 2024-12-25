d1882cbaf41a12a9bd2832dea03640ad3b1ff11c
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-12-25 18:58:45 +0700

    feat: custom inspector background color attribute

---
+++ Assets/DhafinFawwaz/DhafinFawwaz.AnimationUI/Runtime/Attribute/BGColorAttribute.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System;
+
+namespace DhafinFawwaz.AnimationUI {
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Interface | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+	public class BGColorAttribute : Attribute {
+
+		public Color Color { get; }
+
+		public BGColorAttribute (string colorCode) {
+			UnityEngine.ColorUtility.TryParseHtmlString(colorCode, out Color col);
+			Color = col;
+		}
+
+		public static bool TryFindThisOrAnyParentContainBGColorAttribute(Type type, out BGColorAttribute attr) {
+			if(type == null) {
+				attr = null;
+				return false;
+			}
+			attr = Attribute.GetCustomAttribute(type, typeof(BGColorAttribute)) as BGColorAttribute;
+			if(attr != null) return true;
+			return TryFindThisOrAnyParentContainBGColorAttribute(type.BaseType, out attr);
+		}
+	}
+}	
