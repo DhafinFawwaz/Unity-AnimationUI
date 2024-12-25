aea9a310c3ca35da515b8297cffeda4eb3cd6abb
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-12-25 18:55:09 +0700

    feat: custom sub class selector attribute

---
+++ Assets/DhafinFawwaz/DhafinFawwaz.AnimationUI/Runtime/Attribute/SubclassSelectorAnimationUIAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+namespace DhafinFawwaz.AnimationUI {
+	/// <summary>
+	/// Attribute to specify the type of the field serialized by the SerializeReference attribute in the inspector.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+	public sealed class SubclassSelectorAnimationUIAttribute : SubclassSelectorAttribute
+	{}
+}
