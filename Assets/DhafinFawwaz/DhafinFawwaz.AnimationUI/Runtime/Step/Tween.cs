eaff02226eb8c88691529e921b9ab55bda5dc579
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2024-12-27 18:24:10 +0700

    feat: tweening

---
+++ Assets/DhafinFawwaz/DhafinFawwaz.AnimationUI/Runtime/Step/Tween.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using System;
+
+namespace DhafinFawwaz.AnimationUI {
+
+    /// <summary>
+    /// Class that will interpolate a value from From to To and apply it to a certain object property.
+    /// </summary>
+    /// <typeparam name="TComponent">Component target that will have its property interpolated</typeparam>
+    /// <typeparam name="UFrom">From value or component value</typeparam>
+    /// <typeparam name="VTo">To value or component value</typeparam>
+    /// <typeparam name="WInterpolationOutput">Interpolation function return type</typeparam>
+    [BGColor("#ff000015")]
+    public abstract class Tween<TComponent, UFrom, VTo, WInterpolationOutput> : Step, ITweenable, IReverseSequenceHandler where TComponent : Component
+    {
+        public TComponent Target;
+        public Component GetTarget() => Target;
+        public float Duration = 0.5f;
+        public float GetDuration() => Duration;
+        public Ease EaseType = Ease.OutQuart;
+        public UFrom From;
+        public VTo To;
+        public abstract void ApplyInterpolation(WInterpolationOutput value);
+        public void ApplyInterpolationSafe(WInterpolationOutput value) {
+            if(!Target) return;
+            ApplyInterpolation(value);
+        }
+        
+        public void UpdateToFrom() {
+            ApplyInterpolationSafe(_lerpFunction(From, To, 0));
+#if UNITY_EDITOR
+            EditorOnlyProgress = 0;
+#endif
+        }   
+        public void UpdateToTo() {
+            ApplyInterpolationSafe(_lerpFunction(From, To, 1));
+#if UNITY_EDITOR
+            EditorOnlyProgress = 1;
+#endif
+        }        
+        public void Update(float time) => UpdateNormalized(time/Duration);
+        public void UpdateNormalized(float t) {
+            ApplyInterpolationSafe(_lerpFunction(From, To, EaseFunction.Evaluate(t, EaseType)));
+#if UNITY_EDITOR
+            EditorOnlyProgress = t;
+#endif
+        }
+
+        public void SetTargetValueAsFromSafe() {
+            if(!Target) {
+                Debug.LogErrorFormat($"Target is null. Please assign a target first.", Target);
+                return;
+            }
+            SetTargetValueAsFrom();
+        }
+        public void SetTargetValueAsToSafe() {
+            if(!Target) {
+                Debug.LogErrorFormat($"Target is null. Please assign a target first.", Target);
+                return;
+            }
+            SetTargetValueAsTo();
+        }
+        
+        public abstract void SetTargetValueAsFrom();
+        public abstract void SetTargetValueAsTo();
+
+        public void OnSequenceReversed() {
+            if (typeof(UFrom) == typeof(VTo))
+            {
+                object temp = From;
+                From = (UFrom)(object)To;
+                To = (VTo)temp;
+            }
+        }
+
+        public override string GetDisplayName(){
+            if(Target == null) return $"[null] [{Duration} s]";
+            return $"[{Target.gameObject.name}] [{Duration} s]";
+        }
+
+        Func<UFrom, VTo, float, WInterpolationOutput> _lerpFunction;
+        protected Tween() {
+            _lerpFunction = InterpolationFunction;
+        }
+        protected abstract Func<UFrom, VTo, float, WInterpolationOutput> InterpolationFunction {get;}
+        
+        
+    }
+}
