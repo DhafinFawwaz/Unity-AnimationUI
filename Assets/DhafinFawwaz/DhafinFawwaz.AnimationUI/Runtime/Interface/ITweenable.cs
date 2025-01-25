f594dfe9111889367179eb9f2be13c493f9409d4
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2025-01-25 18:30:51 +0700

    chore: remove getter because the target can be derived from any class

--- Assets/DhafinFawwaz/DhafinFawwaz.AnimationUI/Runtime/Interface/ITweenable.cs
+++ Assets/DhafinFawwaz/DhafinFawwaz.AnimationUI/Runtime/Interface/ITweenable.cs
@@ -5,7 +5,6 @@ namespace DhafinFawwaz.AnimationUI {
     // interface for component that will have a target with tweenable value with from and to value
     public interface ITweenable
     {
-        public Component GetTarget();
         public float GetDuration();
         public void Update(float time);
         public void UpdateToFrom();
