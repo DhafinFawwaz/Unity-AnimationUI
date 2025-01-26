612ac8e67c4e7e48b699058c915292e68a117101
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2025-01-26 13:53:55 +0700

    fix: button color when playing & not being able to select step because of inspector lock

--- Assets/DhafinFawwaz/DhafinFawwaz.AnimationUI/Editor/AnimationUIInspector.cs
+++ Assets/DhafinFawwaz/DhafinFawwaz.AnimationUI/Editor/AnimationUIInspector.cs
@@ -6,12 +6,9 @@ namespace DhafinFawwaz.AnimationUI {
     public class AnimationUIInspector : Editor
     {
         AnimationUI _target;
-        GUIStyle redButton;
 
         void OnEnable() {
             _target = target as AnimationUI;
-            GUIStyle redButton = new GUIStyle(GUI.skin.button);
-            redButton.normal.background = Create1pxTexture(new Color(1,0,0,0.3f));
         }
 
         Texture2D Create1pxTexture(Color color) {
@@ -56,12 +53,18 @@ namespace DhafinFawwaz.AnimationUI {
 
             // Play Button
             float totalDuration = _target.CalculateTotalDuration();
-            if(_target.CurrentAnimationTime == 0 && !_target.IsPlaying && GUILayout.Button("Play")) {
+            if(_target.Sequence.Count == 0) {
+                if(GUILayout.Button("Play")) _target.Play(); // If i put GUILayout.Button("Play") in the previous if statement with &&, it will show 2 Play button and 1 Replay Button. Don't complain about this.
+            }
+            else if(_target.CurrentAnimationTime == 0 && !_target.IsPlaying && GUILayout.Button("Play")) {
                 _target.Play();
             } else if(_target.CurrentAnimationTime > 0 && _target.CurrentAnimationTime < totalDuration && !_target.IsPlaying && GUILayout.Button("Resume")) {
                 _target.Resume();
-            } else if(_target.IsPlaying && _target.CurrentAnimationTime < totalDuration && GUILayout.Button("Pause", redButton)) {
-                _target.Pause();
+            } else if(_target.IsPlaying && _target.CurrentAnimationTime < totalDuration ) {
+                Color temp = GUI.backgroundColor;
+                GUI.backgroundColor = Color.red;
+                if(GUILayout.Button("Pause")) _target.Pause();
+                GUI.backgroundColor = temp;
             } else if(!_target.IsPlaying && _target.CurrentAnimationTime >= totalDuration && GUILayout.Button("Replay")) {
                 _target.Play();
             }
@@ -105,11 +108,11 @@ namespace DhafinFawwaz.AnimationUI {
             
             EditorGUILayout.BeginHorizontal();
             EditorGUILayout.PropertyField(it, true); // Play on Start
-            bool _isInspectorLocked = IsInspectorLocked();
-            bool newLockState = GUILayout.Toggle(_isInspectorLocked, _isInspectorLocked ? "Locked" : "Unlocked", "Button", GUILayout.Width(100));
-            if (newLockState != _isInspectorLocked) {
-                SetInspectorLock(newLockState);
-            }
+            // bool _isInspectorLocked = IsInspectorLocked();
+            // bool newLockState = GUILayout.Toggle(_isInspectorLocked, _isInspectorLocked ? "Locked" : "Unlocked", "Button", GUILayout.Width(100));
+            // if (newLockState != _isInspectorLocked) {
+            //     SetInspectorLock(newLockState);
+            // }
             EditorGUILayout.EndHorizontal();
 
             it.NextVisible(false);
